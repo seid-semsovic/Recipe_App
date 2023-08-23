167eedbb1d7588b6b4780ab94a3dad801014e08b
seid-semsovic <semsovicseid@gmail.com>
2023-08-23 14:24:51 +0200

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,8 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd4a51de3-d60b-4066-a615-ef10dd7f9d44';
+export const MODAL_CLOSE_SEC = 2.5;
+
+//https://forkify-api.herokuapp.com/api/v2/recipes
+//https://forkify-api.herokuapp.com/api/v2/recipes/
